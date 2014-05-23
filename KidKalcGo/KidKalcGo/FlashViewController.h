b8bf762983f0b8fbfb0a92dd4134ad748de2aae9
ITforMobile <kbailly@mac.com>
2014-05-22 19:20:00 -0500

    new images

--- KidKalcGo/KidKalcGo/FlashViewController.h
+++ KidKalcGo/KidKalcGo/FlashViewController.h
@@ -10,4 +10,90 @@
 
 @interface FlashViewController : UIViewController
 
+{
+    
+    
+    
+    IBOutlet UILabel    *Answera1;
+    IBOutlet UILabel    *Answera2;
+    IBOutlet UILabel    *Answera3;
+    IBOutlet UILabel    *Answera4;
+    IBOutlet UILabel    *Answera5;
+    IBOutlet UILabel    *Answera6;
+    IBOutlet UILabel    *Answera7;
+    IBOutlet UILabel    *Answera8;
+    IBOutlet UILabel    *Answera9;
+    IBOutlet UILabel    *Answera10;
+    IBOutlet UILabel    *Answera11;
+    IBOutlet UILabel    *Answera12;
+    
+    
+    IBOutlet UILabel    *Answerb1;
+    IBOutlet UILabel    *Answerb2;
+    IBOutlet UILabel    *Answerb3;
+    IBOutlet UILabel    *Answerb4;
+    IBOutlet UILabel    *Answerb5;
+    IBOutlet UILabel    *Answerb6;
+    IBOutlet UILabel    *Answerb7;
+    IBOutlet UILabel    *Answerb8;
+    IBOutlet UILabel    *Answerb9;
+    IBOutlet UILabel    *Answerb10;
+    IBOutlet UILabel    *Answerb11;
+    IBOutlet UILabel    *Answerb12;
+    
+    
+    IBOutlet UILabel    *Answerc1;
+    IBOutlet UILabel    *Answerc2;
+    IBOutlet UILabel    *Answerc3;
+    IBOutlet UILabel    *Answerc4;
+    IBOutlet UILabel    *Answerc5;
+    IBOutlet UILabel    *Answerc6;
+    IBOutlet UILabel    *Answerc7;
+    IBOutlet UILabel    *Answerc8;
+    IBOutlet UILabel    *Answerc9;
+    IBOutlet UILabel    *Answerc10;
+    IBOutlet UILabel    *Answerc11;
+    IBOutlet UILabel    *Answerc12;
+    
+    
+    IBOutlet UILabel    *Answerd1;
+    IBOutlet UILabel    *Answerd2;
+    IBOutlet UILabel    *Answerd3;
+    IBOutlet UILabel    *Answerd4;
+    IBOutlet UILabel    *Answerd5;
+    IBOutlet UILabel    *Answerd6;
+    IBOutlet UILabel    *Answerd7;
+    IBOutlet UILabel    *Answerd8;
+    IBOutlet UILabel    *Answerd9;
+    IBOutlet UILabel    *Answerd10;
+    IBOutlet UILabel    *Answerd11;
+    IBOutlet UILabel    *Answerd12;
+    
+    
+    IBOutlet UILabel    *Answere1;
+    IBOutlet UILabel    *Answere2;
+    IBOutlet UILabel    *Answere3;
+    IBOutlet UILabel    *Answere4;
+    IBOutlet UILabel    *Answere5;
+    IBOutlet UILabel    *Answere6;
+    IBOutlet UILabel    *Answere7;
+    IBOutlet UILabel    *Answere8;
+    IBOutlet UILabel    *Answere9;
+    IBOutlet UILabel    *Answere10;
+    IBOutlet UILabel    *Answere11;
+    IBOutlet UILabel    *Answere12;
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
+
+
+
+
 @end
